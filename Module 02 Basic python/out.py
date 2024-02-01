69e1da622d8bc1b52ca6f2e1a4c65d5ef1f25bfe
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-02-01 22:33:38 +0600

    module-2

--- Module 02 Basic python/out.py
+++ Module 02 Basic python/out.py
@@ -1,4 +1,4 @@
 def answer(name,title, age,clas):
     return (f'My Name is {name} {title}.I am {age} years old. I read in class {clas} ')
 
-# ans = answer("Merazul","Islam",20,13)
+ans = answer("Merazul","Islam",20,13)
