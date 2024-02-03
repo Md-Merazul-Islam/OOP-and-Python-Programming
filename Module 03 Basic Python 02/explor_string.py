e8a8026a7c1fa6e2b9a4dc695f424a34d27b9bf6
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-02-03 11:34:30 +0600

    python

---
+++ Module 03 Basic Python 02/explor_string.py
@@ -0,0 +1,26 @@
+name = 'Md Merazul\'s Islam .' #escape
+name2 = "Meraz's "
+name3 = """ 
+        My name is Md Meraz.
+        I'm 20. And I am a competitive Programmer
+        i love to solve problem using c++ languge 
+        
+""" 
+print(name)
+print(name2)
+print(name3)
+
+print(name.upper())
+print(name.lower())
+print(name.count('a'))
+
+mylist = 'Meraz'
+print(','.join(mylist))
+
+name = "Meraz"
+roll = 672581
+msg = 'my name is {0}. I am {1}.My fvrt number is {1}'.format(name,roll)
+print(msg)
+
+txt ='I mobile is {ag} tk'
+print(txt.format(ag=300))
